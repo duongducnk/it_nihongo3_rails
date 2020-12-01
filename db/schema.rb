f1d1a3628c9c3153b0ae19237fcf9d50c169be54
Ubuntu <ubuntu@ip-172-31-84-190.ec2.internal>
2020-12-01 02:37:48 +0000

    Add review book

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,14 @@ ActiveRecord::Schema.define(version: 2020_12_01_020041) do
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
+    
+  create_table "book_reviews", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+    t.string "review"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
