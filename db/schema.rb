03e67968f7c6ce496373d1e06a51929c5e384afa
Ubuntu <ubuntu@ip-172-31-84-190.ec2.internal>
2020-12-01 02:43:09 +0000

    Add review book

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_12_01_020041) do
-
+ActiveRecord::Schema.define(version: 2020_12_01_022223) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -34,7 +32,7 @@ ActiveRecord::Schema.define(version: 2020_12_01_020041) do
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
-    
+
   create_table "book_reviews", force: :cascade do |t|
     t.integer "user_id"
     t.integer "book_id"
@@ -59,11 +57,6 @@ ActiveRecord::Schema.define(version: 2020_12_01_020041) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-<<<<<<< HEAD
-    
-
-=======
->>>>>>> add_detail_book
     t.string "name"
     t.integer "age"
     t.index ["email"], name: "index_users_on_email", unique: true
